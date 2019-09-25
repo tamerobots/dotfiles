df3b0b8ccfc40829eeae38982929efb2a88c1e98
TameRobots <dave@tamerobots.com>
2019-09-25 21:52:52 +0100

    included .gitconfig

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -12,7 +12,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".bashrc .profile .inputrc"    # list of files/folders to symlink in homedir
+files=".bashrc .profile .inputrc .gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -27,8 +27,8 @@ cd $dir
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+echo "Moving any existing dotfiles from ~ to $olddir"
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/$file
